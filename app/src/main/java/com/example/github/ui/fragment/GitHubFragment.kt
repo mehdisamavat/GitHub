8425db23f5a324075e7b02b9ca3ad80cf7f80777
mehdisamavat <mehdisamavat01@gmail.com>
2023-02-07 04:15:34 +0330

    change to java 11 and add viewpageradapter

--- app/src/main/java/com/example/github/ui/fragment/GitHubFragment.kt
+++ app/src/main/java/com/example/github/ui/fragment/GitHubFragment.kt
@@ -5,20 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.example.github.R
+import com.example.github.databinding.FragmentGitHubBinding
+import com.example.github.ui.fragment.adapter.GitHubViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class GitHubFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GitHubFragment()
+
+    private lateinit var binding: FragmentGitHubBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_git_hub, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_git_hub, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        val viewPagerAdapter = GitHubViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            val tabNames = listOf("Profile", "Repository")
+            tab.text = tabNames[position]
+        }.attach()
+
     }
 
 
---
+++ app/src/main/java/com/example/github/ui/fragment/adapter/GitHubViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.github.ui.fragment.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.github.ui.fragment.profile.ProfileFragment
+import com.example.github.ui.fragment.repository.RepositoryFragment
+
+class GitHubViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> ProfileFragment()
+            else -> RepositoryFragment()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
