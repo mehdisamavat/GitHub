f699c5759644718a4c55bce4aca63b00fc168240
mehdisamavat <mehdisamavat01@gmail.com>
2023-02-07 04:41:54 +0330

    add toolbar and bookmark item menu

--- app/src/main/java/com/example/github/ui/MainActivity.kt
+++ app/src/main/java/com/example/github/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.github.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import com.example.github.R
 import com.example.github.databinding.ActivityMainBinding
@@ -20,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_toolbar_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.miBookMark -> {
+                        true
+                    }
+                    else -> return false
+                }
+            }
+        })
 
     }
 
